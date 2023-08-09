e61042b9f4db5e4c639f0e09ee3e19e1dedf4b9f
Bianca Power <bianca@biancapower.com>
2023-08-09 15:42:05 +1000

    Handle multiple issue names in branch

--- .github/workflows/extract_issue_key.js
+++ .github/workflows/extract_issue_key.js
@@ -5,31 +5,46 @@ const BRANCH_NAME = process.env.BRANCH_NAME;
 const PROJECT_KEY = process.env.PROJECT_KEY;
 const CURRENT_PR_TITLE = process.env.CURRENT_PR_TITLE;
 
-/* Extract the issue key from the branch name */
-let ISSUE_KEY;
-const regex = new RegExp(`^${PROJECT_KEY}-\\d+`);
+/* Extract the issue keys from the branch name */
+let ISSUE_KEYS;
+const regex = new RegExp(`(${PROJECT_KEY}-\\d+)(\\+${PROJECT_KEY}-\\d+)*`, 'g');
 
-if (BRANCH_NAME.match(regex)) {
-    ISSUE_KEY = BRANCH_NAME.match(regex)[0];
+const matches = BRANCH_NAME.match(regex);
+if (matches && matches[0]) {
+    ISSUE_KEYS = matches[0].split('+');
 }
 
-if (ISSUE_KEY) {
-    console.log(`Extracted ISSUE_KEY: ${ISSUE_KEY}`);
-    console.log(`::set-output name=jira_issue_key::${ISSUE_KEY}`);
+let output;
+if (ISSUE_KEYS && ISSUE_KEYS.length > 0) {
+    console.log(`Extracted ISSUE_KEYS: ${ISSUE_KEYS.join(', ')}`);
+
+    if (ISSUE_KEYS.length === 1) {
+        output = ISSUE_KEYS[0];
+    } else {
+        output = `[${ISSUE_KEYS.join(', ')}]`;
+    }
+
+    console.log(`::set-output name=jira_issue_key::${output}`);
 } else {
-    console.log("No ISSUE_KEY found in BRANCH_NAME.");
+    console.log("No ISSUE_KEYS found in BRANCH_NAME.");
 }
 
-/* Check if the PR title starts with ISSUE_KEY followed by ':' */
-if (CURRENT_PR_TITLE.startsWith(`${ISSUE_KEY}:`)) {
-    console.log(`PR title starts with ISSUE_KEY: ${ISSUE_KEY}`);
-    console.log(`::set-output name=pr_title_starts_with_issue_key::true`);
-} else if (CURRENT_PR_TITLE.match(regex)) { // check if PR title starts with a different issue key from the one in the branch name
-    console.log(`PR title starts with an ISSUE_KEY other than ${ISSUE_KEY}: ${CURRENT_PR_TITLE.match(regex)[0]}`);
-    console.log(`::set-output name=pr_title_starts_with_issue_key::false`);
-    console.log(`::set-output name=pr_title_starts_with_other_issue_key::true`);
+/* Check if the PR title starts with one of the ISSUE_KEYS followed by ':' */
+const issueKeysInTitle = (CURRENT_PR_TITLE.match(regex) || []).flatMap(key => key.split('+'));
+
+if (issueKeysInTitle.length > 0) {
+    const isExactMatch = issueKeysInTitle.every(key => ISSUE_KEYS.includes(key)) && issueKeysInTitle.length === ISSUE_KEYS.length;
+
+    if (isExactMatch) {
+        console.log(`PR title starts with the exact ISSUE_KEYS: [${issueKeysInTitle.join(', ')}]`);
+        console.log(`::set-output name=pr_title_starts_with_issue_key::true`);
+    } else {
+        console.log(`PR title contains different ISSUE_KEYS: [${issueKeysInTitle.join(', ')}]`);
+        console.log(`::set-output name=pr_title_starts_with_issue_key::false`);
+        console.log(`::set-output name=pr_title_starts_with_other_issue_key::true`);
+    }
 } else {
     console.log(`PR title: ${CURRENT_PR_TITLE}`);
-    console.log(`PR title does not start with ISSUE_KEY: ${ISSUE_KEY}`);
+    console.log(`PR title does not start with any of the ISSUE_KEYS: ${ISSUE_KEYS && ISSUE_KEYS.length > 0 ? `[${ISSUE_KEYS.join(', ')}]` : ''}`);
     console.log(`::set-output name=pr_title_starts_with_issue_key::false`);
 }
