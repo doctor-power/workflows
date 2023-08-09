d0d245ca1196cad5d2aaf5b1e70b35756f58f59d
Bianca Power <bianca@biancapower.com>
2023-08-09 13:32:14 +1000

    Log output

--- .github/workflows/extract_issue_key.js
+++ .github/workflows/extract_issue_key.js
@@ -3,7 +3,9 @@ dotenv.config();
 
 const BRANCH_NAME = process.env.BRANCH_NAME;
 const PROJECT_KEY = process.env.PROJECT_KEY;
+const CURRENT_PR_TITLE = process.env.CURRENT_PR_TITLE;
 
+/* Extract the issue key from the branch name */
 let ISSUE_KEY;
 const regex = new RegExp(`^${PROJECT_KEY}-\\d+`);
 
@@ -19,4 +21,14 @@ if (ISSUE_KEY) {
     console.log(`PROJECT: ${PROJECT_KEY}`);
     console.log(`ISSUE_KEY: ${ISSUE_KEY || 'undefined'}`);
     console.log("No ISSUE_KEY found in BRANCH_NAME.");
+}
+
+/* Check if the PR title starts with ISSUE_KEY followed by ':' */
+if (CURRENT_PR_TITLE.startsWith(`${ISSUE_KEY}:`)) {
+    console.log(`PR title starts with ISSUE_KEY: ${ISSUE_KEY}`);
+    console.log(`echo "pr_title_starts_with_issue_key=true" >> $GITHUB_OUTPUT`);
+} else {
+    console.log(`PR title: ${CURRENT_PR_TITLE}`);
+    console.log(`PR title does not start with ISSUE_KEY: ${ISSUE_KEY}`);
+    console.log(`echo "pr_title_starts_with_issue_key=false" >> $GITHUB_OUTPUT`);
 }
