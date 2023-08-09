92f671d93395b7eb19b636389d6531630e9a58aa
Bianca Power <bianca@biancapower.com>
2023-08-09 11:36:36 +1000

    Debug

--- .github/workflows/extract_issue_key.sh
+++ .github/workflows/extract_issue_key.sh
@@ -5,12 +5,11 @@
 
 ISSUE_KEY=$(echo $BRANCH_NAME | grep -oE "^$PROJECT-\d+")
 
-echo "BRANCH_NAME: $BRANCH_NAME"
-echo "PROJECT: $PROJECT"
-echo "ISSUE_KEY: $ISSUE_KEY"
-
 if [[ -n $ISSUE_KEY ]]; then # if $ISSUE_KEY is not empty
     echo "Extracted ISSUE_KEY: $ISSUE_KEY"
 else
+    echo "BRANCH_NAME: $BRANCH_NAME"
+    echo "PROJECT: $PROJECT"
+    echo "ISSUE_KEY: $ISSUE_KEY"
     echo "No ISSUE_KEY found in BRANCH_NAME."
 fi
