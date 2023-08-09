bfbdf1697815f72c6afdb1e906dd39ca9ae82177
Bianca Power <bianca@biancapower.com>
2023-08-09 13:07:38 +1000

    Fix JS

--- .github/workflows/extract_issue_key.js
+++ .github/workflows/extract_issue_key.js
@@ -1,5 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const BRANCH_NAME = process.env.BRANCH_NAME;
+const PROJECT_KEY = process.env.PROJECT_KEY;
+
 let ISSUE_KEY;
-const regex = new RegExp(`^${PROJECT}-\\d+`);
+const regex = new RegExp(`^${PROJECT_KEY}-\\d+`);
 
 if (BRANCH_NAME.match(regex)) {
     ISSUE_KEY = BRANCH_NAME.match(regex)[0];
@@ -9,7 +15,7 @@ if (ISSUE_KEY) {
     console.log(`Extracted ISSUE_KEY: ${ISSUE_KEY}`);
 } else {
     console.log(`BRANCH_NAME: ${BRANCH_NAME}`);
-    console.log(`PROJECT: ${PROJECT}`);
+    console.log(`PROJECT: ${PROJECT_KEY}`);
     console.log(`ISSUE_KEY: ${ISSUE_KEY || 'undefined'}`);
     console.log("No ISSUE_KEY found in BRANCH_NAME.");
 }
