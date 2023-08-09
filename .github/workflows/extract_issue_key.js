fd1c88e0cc1cd390046b5bf66a3016f7ebcfd921
Bianca Power <bianca@biancapower.com>
2023-08-09 12:58:55 +1000

    Use JS script instead of bash

---
+++ .github/workflows/extract_issue_key.js
@@ -0,0 +1,18 @@
+// BRANCH_NAME="WA-24_my-branch-name"
+// PROJECT="WA"
+
+let ISSUE_KEY;
+const regex = new RegExp(`^${PROJECT}-\\d+`);
+
+if (BRANCH_NAME.match(regex)) {
+    ISSUE_KEY = BRANCH_NAME.match(regex)[0];
+}
+
+if (ISSUE_KEY) {
+    console.log(`Extracted ISSUE_KEY: ${ISSUE_KEY}`);
+} else {
+    console.log(`BRANCH_NAME: ${BRANCH_NAME}`);
+    console.log(`PROJECT: ${PROJECT}`);
+    console.log(`ISSUE_KEY: ${ISSUE_KEY || 'undefined'}`);
+    console.log("No ISSUE_KEY found in BRANCH_NAME.");
+}
