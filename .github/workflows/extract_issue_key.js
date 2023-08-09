5b5ab011ceedf2d01acc144111566808e6f3885c
Bianca Power <bianca@biancapower.com>
2023-08-09 15:01:04 +1000

    Fail when issue key not in branch name || issue key in PR title is inconsistent with branch name issue key

--- .github/workflows/extract_issue_key.js
+++ .github/workflows/extract_issue_key.js
@@ -15,7 +15,6 @@ if (BRANCH_NAME.match(regex)) {
 
 if (ISSUE_KEY) {
     console.log(`Extracted ISSUE_KEY: ${ISSUE_KEY}`);
-    // console.log(`echo "jira_issue_key=${ISSUE_KEY}" >> $GITHUB_OUTPUT`);
     console.log(`::set-output name=jira_issue_key::${ISSUE_KEY}`);
 } else {
     console.log("No ISSUE_KEY found in BRANCH_NAME.");
@@ -24,11 +23,13 @@ if (ISSUE_KEY) {
 /* Check if the PR title starts with ISSUE_KEY followed by ':' */
 if (CURRENT_PR_TITLE.startsWith(`${ISSUE_KEY}:`)) {
     console.log(`PR title starts with ISSUE_KEY: ${ISSUE_KEY}`);
-    // console.log(`echo "pr_title_starts_with_issue_key=true" >> $GITHUB_OUTPUT`);
     console.log(`::set-output name=pr_title_starts_with_issue_key::true`);
+} else if (CURRENT_PR_TITLE.match(regex)) { // check if PR title starts with a different issue key from the one in the branch name
+    console.log(`PR title starts with an ISSUE_KEY other than ${ISSUE_KEY}: ${CURRENT_PR_TITLE.match(regex)[0]}`);
+    console.log(`::set-output name=pr_title_starts_with_issue_key::false`);
+    console.log(`::set-output name=pr_title_starts_with_other_issue_key::true`);
 } else {
     console.log(`PR title: ${CURRENT_PR_TITLE}`);
     console.log(`PR title does not start with ISSUE_KEY: ${ISSUE_KEY}`);
-    // console.log(`echo "pr_title_starts_with_issue_key=false" >> $GITHUB_OUTPUT`);
     console.log(`::set-output name=pr_title_starts_with_issue_key::false`);
 }
