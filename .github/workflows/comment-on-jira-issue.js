567cb8806e2d1aa3ed6ae0ed2cf9909d7251d749
Bianca Power <bianca@biancapower.com>
2023-08-10 13:11:12 +1000

    Handle bold & headings

--- .github/workflows/comment-on-jira-issue.js
+++ .github/workflows/comment-on-jira-issue.js
@@ -18,19 +18,58 @@ PR_BODY = PR_BODY.split('# Checklist')[0];
 // Replace <img> tags with the plain url
 PR_BODY = PR_BODY.replace(/<img[^>]*src="([^"]*)"[^>]*>/g, '$1');
 
+const createContentItem = (text, type = "text", marks = []) => ({
+  "text": text,
+  "type": type,
+  "marks": marks
+});
+
 // Split PR_BODY by newlines and filter out empty items
 const contentItems = PR_BODY.split('\n')
-    .filter(item => item.trim() !== "")  // filter out empty or whitespace-only strings
-    .map(item => {
-        return {
-            "content": [{
-                "text": item,
-                "type": "text"
-            }],
-            "type": "paragraph"
-        };
-    });
+  .filter(item => item.trim() !== "")  // filter out empty or whitespace-only strings
+  .map(item => {
+      let content = [];
 
+      // Handle bold (Markdown: **bold**)
+      let lastIndex = 0;
+      let boldMatch;
+      const boldRegex = /\*\*([^*]+)\*\*/g;
+
+      while ((boldMatch = boldRegex.exec(item)) !== null) {
+          if (boldMatch.index !== lastIndex) {
+              content.push(createContentItem(item.substring(lastIndex, boldMatch.index)));
+          }
+          content.push(createContentItem(boldMatch[1], "text", [{"type": "strong"}]));
+          lastIndex = boldMatch.index + boldMatch[0].length;
+      }
+
+      if (lastIndex !== item.length) {
+          content.push(createContentItem(item.substring(lastIndex)));
+      }
+
+      if (item.startsWith('## ')) {
+          return {
+              "content": content,
+              "type": "heading",
+              "attrs": {
+                  "level": 2
+              }
+          };
+      } else if (item.startsWith('# ')) {
+          return {
+              "content": content,
+              "type": "heading",
+              "attrs": {
+                  "level": 1
+              }
+          };
+      } else {
+          return {
+              "content": content,
+              "type": "paragraph"
+          };
+      }
+  });
 
 // console.log(contentItems[0]);
 
