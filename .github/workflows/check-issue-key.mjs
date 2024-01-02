f896f10f0600f606c40623e1d5c972e6901ddf26
Bianca Power <bianca@biancapower.com>
2024-01-02 14:06:18 +1100

    Handle release branch name without issue key

--- .github/workflows/check-issue-key.mjs
+++ .github/workflows/check-issue-key.mjs
@@ -9,30 +9,42 @@ const CURRENT_PR_TITLE = process.env.CURRENT_PR_TITLE;
 let ISSUE_KEYS;
 const regex = new RegExp(`(${PROJECT_KEY}-\\d+)(\\+${PROJECT_KEY}-\\d+)*`, 'g');
 
-const matches = BRANCH_NAME.match(regex);
-if (matches && matches[0]) {
-    ISSUE_KEYS = matches[0].split('+');
-}
-
-let output;
-if (ISSUE_KEYS && ISSUE_KEYS.length > 0) {
-    console.log(`Extracted ISSUE_KEYS: ${ISSUE_KEYS.join(', ')}`);
-
-    if (ISSUE_KEYS.length === 1) {
-        output = ISSUE_KEYS[0];
-    } else {
-        output = `[${ISSUE_KEYS.join(', ')}]`;
+if (BRANCH_NAME == 'release') {
+    console.log("Release branch detected. Skipping check for issue key in branch name.");
+    return;
+} else {
+    const matches = BRANCH_NAME.match(regex);
+    if (matches && matches[0]) {
+        ISSUE_KEYS = matches[0].split('+');
     }
 
-    console.log(`::set-output name=jira_issue_key::${output}`);
-} else {
-    console.log("No ISSUE_KEYS found in BRANCH_NAME.");
+    let output;
+    if (ISSUE_KEYS && ISSUE_KEYS.length > 0) {
+        console.log(`Extracted ISSUE_KEYS: ${ISSUE_KEYS.join(', ')}`);
+
+        if (ISSUE_KEYS.length === 1) {
+            output = ISSUE_KEYS[0];
+        } else {
+            output = `[${ISSUE_KEYS.join(', ')}]`;
+        }
+
+        console.log(`::set-output name=jira_issue_key::${output}`);
+    } else {
+        console.log("No ISSUE_KEYS found in BRANCH_NAME.");
+    }
 }
 
 /* Check if the PR title starts with the ISSUE_KEYS followed by ':' */
 const issueKeysInTitle = (CURRENT_PR_TITLE.match(regex) || []).flatMap(key => key.split('+'));
 
 if (issueKeysInTitle.length > 0) {
+    if (BRANCH_NAME == 'release') {
+        console.log(`Release branch detected, and PR title starts with issue key(s): [${issueKeysInTitle.join(', ')}]`);
+        console.log(`::set-output name=pr_title_starts_with_issue_key::true`);
+        return;
+    }
+
+    // If not a release branch, check if the PR title starts with the ISSUE_KEYS from the branch name
     const isExactMatch = issueKeysInTitle.every(key => ISSUE_KEYS.includes(key)) && issueKeysInTitle.length === ISSUE_KEYS.length;
 
     if (isExactMatch) {
